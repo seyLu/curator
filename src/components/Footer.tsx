a8ee5dc862bda7532f83cedcb460c302bc99dc57
MJ Ulysses Sabit <98249191+seyLu@users.noreply.github.com>
2025-10-28 00:03:14 +0800

    feat: update footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,17 @@ import { Anchor } from "@mantine/core";
 
 export function Footer() {
     return (
-        <div className="mx-auto mt-1 flex gap-1">
-            Made with ♥️ by
-            <Anchor href="https://github.com/seylu" target="_blank" underline="always">
-                seylu
-            </Anchor>
+        <div className="mx-auto mt-1">
+            <div className="flex gap-1">
+                Made with ♥️ by
+                <Anchor
+                    href="https://github.com/seyLu/curator"
+                    target="_blank"
+                    underline="always"
+                >
+                    seyLu
+                </Anchor>
+            </div>
         </div>
     );
 }
